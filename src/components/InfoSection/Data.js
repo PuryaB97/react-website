1918953c72d8fc6b927b399c69c4850afbb35386
Purya Behdarvandan <purya.B97@gmail.com>
2020-10-16 21:01:59 +0300

    main

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Tranactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited tranactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-4.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headLine: "Unlimited Tranactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited tranactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-1.svg"),
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headLine: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-2.svg"),
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
